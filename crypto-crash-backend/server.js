322101c422e6fd137f03a7b2c27ae8b7a2ebcbda
Manas Choudhary <manasschoudhary@gmail.com>
2025-08-04 16:26:06 +0530

    🚀 Updated server.js with latest fixes1

--- crypto-crash-backend/server.js
+++ crypto-crash-backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", require("./routes/api"));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://crash-game-backend-six.vercel.app/"], 
+    origin: ["https://crash-game-backend-manaschoudhary23s-projects.vercel.app/"], 
     methods: ["GET", "POST"]
   }
 });
